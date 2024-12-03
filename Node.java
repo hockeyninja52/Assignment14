2bf4562574428737fc310b6d99df1cb77d8a1990
hockeyninja52 <concordiacardinals23@gmail.com>
2024-12-02 22:41:20 -0500

    Node.java

---
+++ Node.java
@@ -0,0 +1,14 @@
+package wk13d3;
+
+public class Node {
+	public Node NextNode;
+	public int Key;
+	public String Value;
+	
+	public Node() {
+		this.NextNode = null;
+		this.Key = 0;
+		this.Value = "";
+	}
+
+}
